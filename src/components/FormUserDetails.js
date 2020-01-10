23c125f81abdbb88637d8cecdf83c056a81bb384
Your Name <arteevraina@gmail.com>
2020-01-10 20:39:58 +0530

    update

--- src/components/FormUserDetails.js
+++ src/components/FormUserDetails.js
@@ -1,9 +1,31 @@
 import React from "react"
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider"
+import AppBar from  "material-ui/AppBar";
+import TextField from "material-ui/TextField"
+import RaisedButton from "material-ui/RaisedButton"
 
 class FormUserDetails extends React.Component {
+
+    continue = (event) => {
+        event.preventDefault()
+        this.props.nextStep()
+    }
+
     render() {
+
+        const {values} = this.props
+
         return(
-            <h1>FormUserDetails</h1>
+                <MuiThemeProvider>
+                    <React.Fragment>
+                        <AppBar title = "Enter User Details" />
+                        <TextField hintText="Enter your First Name"
+                                floatingLabelText="First Name"
+                                onChange={this.props.handleChange}
+                                />
+                        <Text
+                    </React.Fragment>
+                </MuiThemeProvider>
         )
     }
 }
